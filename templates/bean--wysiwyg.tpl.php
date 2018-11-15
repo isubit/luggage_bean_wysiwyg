5d37aa9ac566805ed0697549a51833288059c3ad
Carney, Sarah A <sacarney@iastate.edu>
2018-11-15 16:00:58 -0600

    AGEDS-7 Fix template, add option for images

--- templates/bean--wysiwyg.tpl.php
+++ templates/bean--wysiwyg.tpl.php
@@ -2,10 +2,6 @@
 
   <?php print render($title_suffix) ?>
 
-  <?php if ($bean->title !== ''): ?>
-    <h2><?php print $title; ?></h2>
-  <?php endif; ?>
-
   <?php print $bean->field_body['und'][0]['value']; ?>
 
 </div>
